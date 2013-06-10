0bb519e08e23f83c5476dc39ab0ae7d66a04a7ad
Bruno Almeida <Bruno.Sii@live.com>
2013-06-10 16:29:46 -0300

    4º Alteração na camada de Visão xHTML
    Alteração do Primeiro CRUD
    Versão do java 1.7 tambem foi alterada
    para verção 1.6

--- Financeiro/src/main/java/Teste.java
+++ Financeiro/src/main/java/Teste.java
@@ -3,21 +3,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import br.com.financeiro.entidade.Banco;
 
-
 public class Teste {
 
 	/**
 	 * @param args
+	 * @author Bruno.Almeida
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		BeanFactory factory = new ClassPathXmlApplicationContext(
-		        new String[] {"applicationContext.xml"});
-		
-		Banco medico = (Banco)factory.getBean("Medico");
-		System.out.println(medico);
-//		System.out.println(pessoa.getEndereco());
+				new String[] { "applicationContext.xml" });
+
+		Banco banco = (Banco) factory.getBean("Banco");
+		System.out.println(banco);
+		// System.out.println(pessoa.getEndereco());
 
 	}
 
